14c0d2c4bda304cced81d50da29f9130ceb11317
Gerard Rico <g.rico.botella@gmail.com>
2024-04-27 22:23:38 +0200

    fix: quote select command's mailbox argument

--- src/Protocol/Command/Select.php
+++ src/Protocol/Command/Select.php
@@ -2,10 +2,12 @@
 
 namespace Gricob\IMAP\Protocol\Command;
 
+use Gricob\IMAP\Protocol\Command\Argument\QuotedString;
+
 readonly class Select extends Command
 {
     public function __construct(string $mailbox)
     {
-        parent::__construct('SELECT', $mailbox);
+        parent::__construct('SELECT', new QuotedString($mailbox));
     }
 }
